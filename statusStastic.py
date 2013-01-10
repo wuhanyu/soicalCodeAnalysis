b310baad60169ff22932eda0d19f9e71a7b53a6c
wuhanyu <wuhanyu@gmail.com>
2013-01-10 15:34:21 +0800

    add label statistic

--- statusStastic.py
+++ statusStastic.py
@@ -13,7 +13,7 @@ from dateutil.parser import parse
 reload(sys)
 sys.setdefaultencoding(sys.stdout.encoding)
 
-bug_status_statistics = {}
+statisticsSet = {}
 
 def statistics(dict, key):
 	if (key in dict.keys()):
@@ -22,7 +22,13 @@ def statistics(dict, key):
 		dict[key] = 1
 	return dict
 	
+def statisticsByName(name, key):
+	if (name not in statisticsSet.keys()):
+		statisticsSet[name] = {}	
+	statistics(statisticsSet[name], key)
+	
 def printStatistics(dict, name):
+	print name
 	file_object = open(name + '.txt', 'w')
 	sum = 0
 	for item in dict:
@@ -34,6 +40,8 @@ def printStatistics(dict, name):
 	print output
 	file_object.write(output + '\n')
 	file_object.close( )
+	print ''
+	print ''
 
 def processFile(filepath):
 	#print filepath
@@ -44,12 +52,13 @@ def processFile(filepath):
 	if (cmp(title, 'Project hosting on Google Code') != 0):
 		#print page('title').text()
 		bug_status = page('span[@title]').eq(0).text()
-		statistics(bug_status_statistics, bug_status)
-		# if (cmp(bug_status, 'Fixed') == 0 or cmp(bug_status, 'Verified') == 0):
+		statisticsByName('bug_status_statistics', bug_status)
+		if (cmp(bug_status, 'Fixed') == 0 or cmp(bug_status, 'Verified') == 0):
 			# print bug_status
-			# for item in page('a.label'):
-				# print page(item).text()
-			# items = page('tr.cursor_off')
+			for item in page('a.label'):
+				tmp = page(item).text().split('- ')
+				if (len(tmp) == 2): statisticsByName(tmp[0], tmp[1])
+			items = page('tr.cursor_off')
 			# print len(items)
 			
 			# authordiv = page("div.author")
@@ -62,11 +71,12 @@ def processFile(filepath):
 					# time = parse(timestr)
 					# print time - starttime
 
-list_dirs = os.walk("D:/tom/bugs-html")
+list_dirs = os.walk("./test")
 for root, dirs, files in list_dirs:
 	print root
 	for f in files: 
 		processFile(root + "/" + f)
 
-printStatistics(bug_status_statistics, "bug_status_statistics")
+for key in statisticsSet:
+	printStatistics(statisticsSet[key], key)
 #processFile('D:/tom/test/1.html')
