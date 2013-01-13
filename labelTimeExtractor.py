e9a06f343f27c44b8915febafc63e7b9f503c9e6
SimonWu <wuhanyu@gmail.com>
2013-01-13 21:26:58 +0800

    update

--- labelTimeExtractor.py
+++ labelTimeExtractor.py
@@ -10,6 +10,8 @@ import sys
 from pyquery import PyQuery
 from dateutil.parser import parse
 from dateutil.relativedelta import *
+
+
 	
 def getDifTime(starttime, endtime):
 	dif = relativedelta(endtime, starttime)
@@ -27,15 +29,40 @@ def getTime(page):
 	items = page('tr.cursor_off')
 	authordiv = page("div.author")
 	starttime = parse(page(authordiv).find('span.date').attr('title'))
+	count = 0
+	devSet = {}
 	for item in items:
+		count = count + 1
+		#dev-count
+		authorspan = page("span.author")
+		author = authorspan.eq(count).find('a').eq(1).text()
+		
+		if (author): devSet[author] = True
+		#fix-time
 		status = page(item).find('div.box-inner')
 		if (len(status) > 0):
 			tmp = status.text().lower().split('status: ')
 			if (len(tmp) > 1 and (cmp(tmp[1], 'fixed') == 0 or cmp(tmp[1], 'verified') == 0)):
 				timestr = page(item).find('span.date').attr('title')
 				time = parse(timestr)
-				return getDifTime(starttime, time)
+				return getDifTime(starttime, time), count, len(devSet.keys())
 	return None
+	
+def getDevNum(page):
+	items = page('tr.cursor_off')
+	
+def getCommentNum(num):
+	if (num > 100): return 'Com>100'
+	elif (num > 50): return 'Com>50'
+	elif (num > 25): return 'Com>25'
+	elif (num > 10): return 'Com>10'
+	elif (num > 5): return 'Com>5'
+	else: return 'Com=' + str(num)
+	
+def getDevNum(num):
+	if (num > 10): return 'Dev>10'
+	elif (num > 5): return 'Dev>5'
+	else: return 'Dev=' + str(num)
 
 def processFile(filepath):
 	#print filepath
@@ -47,15 +74,32 @@ def processFile(filepath):
 		#print page('title').text()
 		bug_status = page('span[@title]').eq(0).text()
 		if (bug_status and (cmp(bug_status.lower(),'verified') == 0 or cmp(bug_status.lower(),'fixed') == 0)):
-			result = getTime(page)
-			if (result):
+			tmp = getTime(page)
+			if (tmp):
+				reporter = page("div.author").eq(0).find('a').eq(0).text()
+				result = tmp[0]
+				result = result + ' ' + reporter
+				result = result + ' ' + getCommentNum(tmp[1])
+				result = result + ' ' + getDevNum(tmp[2])
 				for item in page('a.label'):
 					labeltext = page(item).text()
 					labeltext = labeltext.lower().replace(' ', '')
-					result = result + ' ' + labeltext
+					if (labeltext.find('pri-') >= 0):
+						result = result + ' ' + labeltext
+					elif (labeltext.find('feature-') >= 0):
+						result = result + ' ' + labeltext
+					elif (labeltext.find('area-') >= 0):
+						result = result + ' ' + labeltext
+					elif (labeltext.find('os-') >= 0):
+						result = result + ' ' + labeltext
+					elif (labeltext.find('mstone-') >= 0):
+						result = result + ' ' + labeltext
+					elif (labeltext.find('size-') >= 0):
+						result = result + ' ' + labeltext
 				print result
 
-#list_dirs = os.walk("./test")
+
+# list_dirs = os.walk("./test")
 list_dirs = os.walk("D:/tom/bugs-html")
 for root, dirs, files in list_dirs:
 	for f in files: 
